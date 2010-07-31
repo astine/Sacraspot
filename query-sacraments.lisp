969476d6f822d18779cdbe0f695cc01961ac5786
Andrew Stine <illuminati1113@gmail.com>
2010-07-31 14:18:54 -0400

    * query-sacraments.lisp: Factored the parameter parsing out of query-sacraments and of query-sacraments-html to a macro they share

--- query-sacraments.lisp
+++ query-sacraments.lisp
@@ -51,35 +51,6 @@
   (yason:with-output-to-string* ()
     (yason:with-array ()
       (doquery (:raw (generate-sacraments-query time distance future maxresults sacraments language latitude longitude))
-	  ;(:limit
-		;(:order-by
-		 ;(:select 'fullname 'city 'state 'sacrament_type 'time 'details 'language 'latitude 'longitude
-			  ;(:as (:ll_distance 'latitude latitude 
-					     ;'longitude longitude) 
-			       ;'distance)
-			  ;(:as (:+ (:* (:ll_distance 'latitude latitude
-						 ;'longitude longitude)
-				       ;6)
-				   ;(:/ (:extract 'epoch (:- 'time time)) 360) )
-			       ;'weight)
-			  ;:from 'events
-			  ;:inner-join 'parishes :on (:= 'events.parish_id 
-							;'parishes.parish_id)
-			  ;:inner-join 'schedules :on (:= 'events.schedule_id 
-							 ;'schedules.schedule_id)
-			  ;:where (:and (:< (:ll_distance 'latitude latitude 
-							 ;'longitude longitude) 
-					   ;distance)
-				       ;(:raw (sql-compile
-					      ;`(:or
-						;,@(mapcar (lambda (sacrament)
-							    ;`(:= 'sacrament_type ,sacrament))
-							  ;sacraments))))
-				       ;(:raw (sql-compile (if language `(:= 'language ,language) t)))
-				       ;(:> 'time time)
-				       ;(:< 'time (timestamp+ time future :sec))))
-		 ;'weight)
-		;maxresults)
 	  (fullname city state kind time details language latitude longitude distance weight)
 	(yason:with-object ()
 	  (yason:encode-object-element "fullname" fullname)
@@ -110,44 +81,36 @@
 	       (:td (str details))
 	       (:td (str (format nil "~R miles" (round distance))))))))))
 
+(defmacro with-sacraments-query-parameters (&body body)
+  "When run within a http callback, fetches and binds the parameters that are expected of
+   a call to query-sacraments"
+  (with-gensyms (lat-long)
+    `(let ((ip (fetch-parameter "ip" (real-remote-addr) nil))
+	   (time (fetch-parameter "time" (now) #'parse-timestring))
+	   (distance (fetch-parameter "distance" 25))
+	   (future (fetch-parameter "future" 453000))
+	   (maxresults (fetch-parameter "maxresults" 25))
+	   (sacraments (fetch-parameter "sacraments" '("Mass" "Confession") #'yason:parse))
+	   (language (fetch-parameter "language")))
+       (let* ((,lat-long (unless (and (parameter "latitude")
+				      (parameter "longitude"))
+			   (latitude-and-longitude ip)))
+	      (latitude (fetch-parameter "latitude" (first ,lat-long)))
+	      (longitude (fetch-parameter "longitude" (second ,lat-long))))
+      ,@body))))
+
 (define-easy-handler (query-sacraments* :uri "/query-sacraments" :default-request-type :post) ()
   "Handles calls to query-sacraments"
   (with-connection *connection-spec*
-    (let ((ip (fetch-parameter "ip" (real-remote-addr) nil))
-	  (time (fetch-parameter "time" (now) #'parse-timestring))
-	  (distance (fetch-parameter "distance" 25))
-	  (future (fetch-parameter "future" 453000))
-	  (maxresults (fetch-parameter "maxresults" 25))
-	  (sacraments (fetch-parameter "sacraments" '("Mass" "Confession") #'yason:parse))
-	  (language (fetch-parameter "language")))
-      (let* ((lat-long (unless (and (parameter "latitude")
-				    (parameter "longitude"))
-			 (latitude-and-longitude ip)))
-	     (latitude (fetch-parameter "latitude" (first lat-long)))
-	     (longitude (fetch-parameter "longitude" (second lat-long)))
-	     )
-	;lat-long))))
-	;(write-to-string (list time distance future maxresults sacraments latitude longitude))))))
-	(query-sacraments time distance future maxresults sacraments language latitude longitude)
-))))
+    (with-sacraments-query-parameters
+      (query-sacraments time distance future maxresults sacraments language latitude longitude))))
 
 (define-easy-handler (query-sacraments-html* :uri "/query-sacraments-html" :default-request-type :post) ()
   "Handles calls to query-sacraments-html"
   (with-connection *connection-spec*
-    (let ((ip (fetch-parameter "ip" (remote-addr*) nil))
-	  (time (fetch-parameter "time" (now) #'parse-timestring))
-	  (distance (fetch-parameter "distance" 25))
-	  (future (fetch-parameter "future" 453000))
-	  (maxresults (fetch-parameter "maxresults" 25))
-	  (sacraments (fetch-parameter "sacraments" '("Mass" "Confession") #'yason:parse))
-	  (language (fetch-parameter "language")))
-      (let* ((lat-long (unless (and (parameter "latitude")
-				    (parameter "longitude"))
-			 (latitude-and-longitude ip)))
-	     (latitude (fetch-parameter "latitude" (first lat-long)))
-	     (longitude (fetch-parameter "longitude" (second lat-long))))
-	(let ((*string-modifier* #'identity))
-	  (with-output-to-string (*standard-output*)
-	    (fill-and-print-template (pathname "/var/www/localhost/htdocs/www/fallback-frontpage.html")
-				     `(:sacraments ,(query-sacraments-html time distance future maxresults sacraments language latitude longitude))
-				     :stream *standard-output*)))))))
+    (with-sacraments-query-parameters
+      (let ((*string-modifier* #'identity))
+	(with-output-to-string (*standard-output*)
+	  (fill-and-print-template (pathname "/var/www/localhost/htdocs/www/fallback-frontpage.html")
+				   `(:sacraments ,(query-sacraments-html time distance future maxresults sacraments language latitude longitude))
+				   :stream *standard-output*))))))
